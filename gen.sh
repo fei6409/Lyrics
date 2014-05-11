1f0785d44ac1110809e09dc38ba0c8ceb43a63ab
fei6409 <fei6409@gmail.com>
2014-05-11 16:46:14 +0800

    fix bugs

--- gen.sh
+++ gen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 (
-echo -e '<html><link rel="stylesheet" type="text/css" href="./style.css"></style>\n<body>\n<h1 align=center>Lyrics Listing</h1>\n<hr/>\n'
+echo -e '<html><link rel="stylesheet" type="text/css" href="./style.css"></link>\n<body>\n<h1 align=center>Lyrics Listing</h1>\n<hr/>\n'
 ls -1 *.html | grep -v "^default\.html$" | grep -v "^index\.html$" | awk '{ printf "<a href=\"%s\">%s</a><br>\n",$0,$0 }'
 echo -e "</body>\n</html>"
 ) > index.html
