1121a464c0aa2ac50cca0a47280059e675b1940f
fei6409 <fei6409@gmail.com>
2017-02-08 16:51:14 +0800

    Some modifies in gen.sh

--- gen.sh
+++ gen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 (
-echo -e '<html><link rel="stylesheet" type="text/css" href="./style.css"></link>\n<body>\n<h1 align=center>Lyrics Listing</h1>\n<hr/>\n'
+echo -e '<html>\n<head>\n\t<meta HTTP-EQUIV="Content-Type" CONTENT="text/html" charset="utf-8">\n\t<link rel="stylesheet" type="text/css" href="./style.css"></link>\n</head>\n<body>\n<h1 align=center>Lyrics Listing</h1>\n<hr/>\n'
 ls -1 *.html | grep -v "^default\.html$" | grep -v "^index\.html$" | awk '{ printf "<a href=\"%s\">%s</a><br>\n",$0,$0 }'
 echo -e "</body>\n</html>"
 ) > index.html
